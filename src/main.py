9bbdaf445f6af6ef4f3262120879550aa36f4126
harshalmore31 <86048671+harshalmore31@users.noreply.github.com>
2024-12-10 14:58:27 +0530

    remove __pycache__

---
+++ src/main.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from functions.processes import process_prompt,copy_to_clipboard, check_api
+from dotenv import load_dotenv
+import os
+
+
+# Streamlit UI Design
+st.set_page_config(page_icon="assests\krya_logo.png",page_title="Krya.ai Automation Console", layout="wide",initial_sidebar_state="expanded")
+
+# Custom CSS for modern look and adjustments
+with open(r"src\UI\style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+# Sidebar with improved styling
+with st.sidebar:
+    # Logo and Project Name
+    st.image("assests\krya_logo.svg", width=50)
+    st.markdown("<h1>Krya AI</h1>", unsafe_allow_html=True)
+
+    google_api_key = st.text_input("Enter your Google API Key", placeholder="e.g., Ax********", type="password")
+    save_button = st.button("Save")
+    if check_api()==True:
+        st.success("API key loaded")
+    else:
+        st.error("Enter the API key")
+    if save_button and google_api_key:
+        with open(".env", "w") as f:
+            f.write(f"GOOGLE_API_KEY={google_api_key}")
+        st.success("Google API Key saved")
+        load_dotenv()    
+
+    st.markdown("### Sample Prompts-")
+    st.markdown("<div class='sample-prompts'>", unsafe_allow_html=True)
+    st.code("Generate a portfolio website on VS CODE", language="text")
+    st.code("Compare Phones Under 20k on different E-commerce websites", language="text")
+    st.code("Type Gmail letter to HR about Leave", language="text")
+    st.code("Create a simple website and host it on AWS", language="text")
+    st.markdown("</div>", unsafe_allow_html=True)
+
+    # Social Links
+    st.markdown("---")
+    st.markdown("       Developed by Devdatta Talele")
+
+    st.markdown("""
+        <div class="social-links">
+            <a href="https://github.com/devdattatalele/krya.ai" target="_blank" class="social-link">
+                <i class="fab fa-github"></i> GitHub
+            </a>
+            <a href="https://linkedin.com/in/devdatta-talele" target="_blank" class="social-link">
+                <i class="fab fa-linkedin"></i> LinkedIn
+            </a>
+        </div>
+    """, unsafe_allow_html=True)
+
+# Main Content Centered
+st.markdown("""
+    <div class="main-container">
+        <h1>KRYA.AI</h1>
+""", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; font-weight:bold; font-family:comic sans ms; padding-top: 0rem;'> \
+            Automated interactions and code execution on local machine </h3>", unsafe_allow_html=True)
+user_input = st.text_input("Enter your command", placeholder="e.g., Open Notepad")
+execute_button = st.button("Execute")
+if execute_button:
+    load_dotenv()
+    if check_api()==True:
+        if execute_button and user_input:
+            with st.spinner("Processing your request..."):
+                code, execution_status = process_prompt(user_input)
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.subheader("Generated Code")
+                    st.code(code, language="python")
+                    copy_to_clipboard(code, "copy_code")
+                with col2:
+                    st.subheader("Execution Status")
+                    st.text_area("", execution_status, height=400, key="execution_status")
+                    copy_to_clipboard(execution_status, "copy_status")
+    else:
+        st.error("API not found, Enter the API in sidebar")
+        st.stop()
+
+
+st.markdown("</div>", unsafe_allow_html=True)
+
