2e75269397769979f88563cbabde5a5c49cce32e
harshalmore31 <86048671+harshalmore31@users.noreply.github.com>
2024-12-10 14:57:51 +0530

    Remove unused files and clean up project structure by deleting obsolete scripts and configuration files

--- archives/Krya_forntend/preload.js
+++
@@ -1,6 +0,0 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    executeCommand: (command) => ipcRenderer.invoke('execute-command', command)
-});
